9643c4cc2f7379e0abcfb48d986d648523e4de0f
Nguyễn Văn Dương 48 (AI) <duongnv48@fpt.com.vn>
2024-03-18 15:53:12 +0700

    update

--- src/android/Ekyc.java
+++ src/android/Ekyc.java
@@ -1,19 +1,30 @@
 package com.fci.plugin;
 
+import android.app.Activity;
 import android.content.Intent;
+
+import com.fpt.fci.ekycfull.EkycResult;
 import com.fpt.fci.ekycfull.domain.BaseConfig;
 import com.fpt.fci.ekycfull.presentation.view.EkycActivity;
-import org.apache.cordova.*;
+import com.google.gson.Gson;
+
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaInterface;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CordovaWebView;
 import org.json.JSONArray;
 import org.json.JSONException;
+
 import java.util.UUID;
 
 public class Ekyc extends CordovaPlugin {
+    private CallbackContext callbackContext;
 
     @Override
     public boolean execute(String action, JSONArray data, CallbackContext callbackContext) {
 
         if (action.equals("startSDK")) {
+            this.callbackContext = callbackContext;
             cordova.getActivity().runOnUiThread(() -> {
                 try {
                     startSDK(data);
@@ -27,8 +38,8 @@ public class Ekyc extends CordovaPlugin {
             return false;
         }
     }
-	
-	private void startSDK (JSONArray data) throws JSONException {
+
+    private void startSDK (JSONArray data) throws JSONException {
         String apiKey = data.getString(0);
         String uuid = data.getString(1);
         int docType = data.getInt(2);
@@ -36,8 +47,18 @@ public class Ekyc extends CordovaPlugin {
         Intent intent = new Intent(cordova.getContext(), EkycActivity.class);
         intent.putExtra(BaseConfig.ParamType.API_KEY.name(), apiKey);
         intent.putExtra(BaseConfig.ParamType.DOCUMENT_TYPE.name(), docType);
+        intent.putExtra(BaseConfig.ParamType.LANG.name(), "vi");
         intent.putExtra(BaseConfig.ParamType.UUID.name(), uuid.isEmpty() ? UUID.randomUUID().toString() : uuid);
         intent.putExtra(BaseConfig.ParamType.ENVIRONMENT.name(), env);
-        cordova.getActivity().startActivity(intent);
+        cordova.startActivityForResult(this, intent, 0);
     }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+        if (resultCode == Activity.RESULT_OK) {
+            this.callbackContext.success(new Gson().toJson(EkycResult.INSTANCE.getOcrData()));
+        }
+    }
+
 }
