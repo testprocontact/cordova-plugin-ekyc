cfd982b54393cb026c611ec03980460e55297367
Nguyễn Văn Dương 48 (AI) <duongnv48@fpt.com.vn>
2024-05-22 16:40:30 +0700

    update_ekyc

--- www/ekyc.js
+++ www/ekyc.js
@@ -1,9 +1,6 @@
 /*global cordova, module*/
 
 module.exports = {
-    startSDK: function (apiKey, uuid, docType, env, successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "Ekyc", "startSDK", [apiKey, uuid, docType, env]);
-    }
 	startSDK: function (apiKey, uuid, docType, env, front, back, successCallback, errorCallback) {
         cordova.exec(successCallback, errorCallback, "Ekyc", "startSDK", [apiKey, uuid, docType, env, front, back]);
     }
